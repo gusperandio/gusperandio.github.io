9c999a4529662eb0d1a0e5a76214b0ab5f397ee9
Gustavo Sperandio <gustavo.sperandio25@gmail.com>
2023-02-28 15:56:41 -0300

    commit

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ function bandeira(e) {
   var desc2 = document.getElementById("desc2");
   var btn1 = document.getElementById("btn1");
   var btn2 = document.getElementById("btn2");
+  var btn3 = document.getElementById("btn3");
   var titleSkill = document.getElementById("skill-section-title");
   var ferramentas = document.getElementById("ferramentas");
   var mobile = document.getElementById("moveis");
-
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("skills-container").style.display = "flex";
+  document.getElementById("certifications").style.display = "none";
   if (e === "brasil") {
     welcome.innerHTML = "Seja bem-vindo!";
     profissao.innerHTML = "Desenvolvedor FullStack";
@@ -20,8 +23,11 @@ function bandeira(e) {
       "Adoro trabalhar com a minha criatividade quando estou programando, o front-end para mim se torna uma tela para pintar onde nem o céu é o limite. Mesmo sabendo que uma pincelada a mais ou a menos pode estragar o quadro, ainda sei os meus limites para tornar algo magnífico.";
     btn1.innerHTML = "Habilidades";
     btn2.innerHTML = "Projetos";
-    titleSkill.innerHTML = "Minhas habilidades";
+    btn3.innerHTML = "Certificações"
+    titleSkill.innerHTML = "Habilidades";
     ferramentas.innerHTML = "Ferramentas";
+    localStorage.setItem("brasil", "ok");
+    localStorage.removeItem("eua");
   } else {
     welcome.innerHTML = "Hello, welcome!";
     profissao.innerHTML = "Fullstack Developer";
@@ -43,7 +49,158 @@ function bandeira(e) {
       "I love working with my creativity when I'm programming, the front-end for me becomes a canvas to paint where not even the sky is the limit. Even knowing that one more or less brushstroke can spoil the painting, I still know my limits to make something magnificent.";
     btn1.innerHTML = "Skills";
     btn2.innerHTML = "Projects";
-    titleSkill.innerHTML = "My skills";
+    btn3.innerHTML = "Certifications"
+    titleSkill.innerHTML = "Skills";
     ferramentas.innerHTML = "Tools";
+    localStorage.setItem("eua", "ok");
+    localStorage.removeItem("brasil");
   }
+}
+
+function projects(e) {
+  let project = document.getElementById("projects");
+  let skills = document.getElementById("skills-container");
+  let certify = document.getElementById("certifications");
+  let titleSkill = document.getElementById("skill-section-title");
+
+  switch (e) {
+    case "C":
+      credentials();
+      certify.style.display = "flex";
+      project.style.display = "none";
+      skills.style.display = "none";
+      if (
+        localStorage.getItem("eua") === "ok" ||
+        !localStorage.getItem("brasil")
+      ) {
+        titleSkill.innerHTML = "Certifications";
+      } else {
+        titleSkill.innerHTML = "Certificações";
+      }
+
+      break;
+    case "P":
+      certify.style.display = "none";
+      project.style.display = "flex";
+      skills.style.display = "none";
+
+      if (
+        localStorage.getItem("eua") === "ok" ||
+        !localStorage.getItem("brasil")
+      ) {
+        titleSkill.innerHTML = "Projects";
+      } else {
+        titleSkill.innerHTML = "Projetos";
+      }
+      break;
+    case "S":
+      certify.style.display = "none";
+      project.style.display = "none";
+      skills.style.display = "flex";
+
+      if (
+        localStorage.getItem("eua") === "ok" ||
+        !localStorage.getItem("brasil")
+      ) {
+        titleSkill.innerHTML = "Skills";
+      } else {
+        titleSkill.innerHTML = "Habilidades";
+      }
+      break;
+  }
+}
+
+function credentials() {
+  const cursos = [
+    {
+      name: "React + Redux",
+      link: "https://www.udemy.com/certificate/UC-00e0db79-f472-46bd-ba7d-112456a16ca6/",
+      desc: "Desenvolvendo front-end com framework JS e gerenciamento de estado.",
+      data: "Dezembro 28, 2022",
+    },
+    {
+      name: "Api RestFUL",
+      link: "https://www.udemy.com/certificate/UC-fe6aba63-08de-4764-98f9-e9dbc375dba3/",
+      desc: "Desenvolvimento de APIs com tecnologias NodeJS, Restify e Mongoose.",
+      data: "Agosto 13, 2022",
+    },
+    {
+      name: "Git/Github",
+      link: "https://www.udemy.com/certificate/UC-fcf98d7d-bc5a-440f-acea-255157e57ca7/",
+      desc: "Versionamento de projetos utilizando Git e Github + Github pages.",
+      data: "Janeiro 16, 2023",
+    },
+    {
+      name: "IONIC 6",
+      link: "https://www.udemy.com/certificate/UC-e7be056c-42c6-48b1-ae9e-6009836ae775/",
+      desc: "Desenvolvimento mobile com Angular + versão framework IONIC.",
+      data: "Julho 12, 2022",
+    },
+    {
+      name: "SCRUM na prática",
+      link: "https://www.udemy.com/certificate/UC-2e22b451-af2c-44fe-940e-f3db8888b397/",
+      desc: "Aplicando metodologia ágil Scrum em hambientes reais e rotineiros.",
+      data: "Fevereiro 17, 2023",
+    },
+    {
+      name: "Desenvolvedor NodeJS",
+      link: "https://www.udemy.com/certificate/UC-3b74b1bf-5b06-408c-a455-802367d8f247/",
+      desc: "Desenvolvimento com NodeJS e banco de dados MongoDB.",
+      data: "Setembro 23, 2020",
+    },
+    {
+      name: "AWS Practitioner",
+      link: "",
+      desc: "DEVOPS - Práticas iniciais com estrutura AWS",
+      data: "Janeiro 6, 2023",
+    },
+  ];
+
+  let text = "";
+  for (let i = 0; i < cursos.length; i++) {
+    text +=
+      `<div class="cardCertify">
+  <div style="display: flex; justify-content: flex-end">
+    <img src="./img/certificado.png" class="imgCertify" />
+  </div>
+  <br />
+  <a
+    href="` +
+      cursos[i].link +
+      `"
+    target="_blank"
+    ><h3 class="cardCertify__title">` +
+      cursos[i].name +
+      `</h3></a
+  >
+  <p class="cardCertify__content">` +
+      cursos[i].desc +
+      `
+  </p>
+  <div class="cardCertify__date">` +
+      cursos[i].data +
+      `</div>
+  <a
+    href="` +
+      cursos[i].link +
+      `"
+    target="_blank"
+    ><div class="cardCertify__arrow">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        height="15"
+        width="15"
+      >
+        <path
+          fill="#fff"
+          d="M13.4697 17.9697C13.1768 18.2626 13.1768 18.7374 13.4697 19.0303C13.7626 19.3232 14.2374 19.3232 14.5303 19.0303L20.3232 13.2374C21.0066 12.554 21.0066 11.446 20.3232 10.7626L14.5303 4.96967C14.2374 4.67678 13.7626 4.67678 13.4697 4.96967C13.1768 5.26256 13.1768 5.73744 13.4697 6.03033L18.6893 11.25H4C3.58579 11.25 3.25 11.5858 3.25 12C3.25 12.4142 3.58579 12.75 4 12.75H18.6893L13.4697 17.9697Z"
+        ></path>
+      </svg></div
+  ></a>
+</div>`;
+  }
+
+  document.getElementById("certifications").innerHTML = text;
 }
